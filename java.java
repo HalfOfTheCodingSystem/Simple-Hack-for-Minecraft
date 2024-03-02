cfe2185911f738117d7192de010f5a243bd40b7f
HalfOfTheProgrammingSystem <161976017+HalfOfTheCodingSystem@users.noreply.github.com>
2024-03-02 22:38:45 +0200

    Create Fly.json
    it`s just fly

---
+++ java.java
@@ -0,0 +1,15 @@
+{
+    "modulename": "FlyModule",
+    "modulekey": 19,
+    "enabled": false,
+    "moduleSettings": {},
+    "moduleValues": {
+        "flyspeed": {
+            "min": 0.7,
+            "max": 20,
+            "value": 0.5,
+            "name": "Fly speed",
+            "shouldRound": false,
+        }
+    }
+}
